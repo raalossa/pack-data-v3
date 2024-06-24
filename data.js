9bd7fd4a34faf25e5a75566f3584f66be65a2a00
raalossa <113949863+raalossa@users.noreply.github.com>
2024-06-24 12:52:32 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,175 @@
+
+// BASES DE DATOS
+const packages = [
+
+    {
+        codigo :"Emp1",
+        nombre: "Envase 2",
+        descripcion:"Cartón impreso dentro de bolsa termosellada transparente de PP",
+        linkImagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxFJJ2UIrrFwA7SteuAgm9RtHPQVtVtIHcFQ&s",
+        
+        componentes: [
+            {
+            papelgUnd :20, 
+            plastgUnd : 93, 
+            vidriogUnd: 50, 
+            metalgUnd: 70, 
+            otrogUnd: 12
+        }
+        ],
+
+        consumos:[{
+            2018 : 5000,
+            2019 : 12500,
+            2020 : 7700,
+            2021 : 2340,
+            2022 : 7500,
+            2023 : 8700,
+            2024 : 2400,
+
+            
+        }]
+    },
+
+    {
+        codigo :"Emp2",
+        nombre: "Envase 3",
+        descripcion:"Cartón impreso dentro de bolsa termosellada transparente de PP",
+        linkImagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6tCrpivHqLKYMBuxe57DGlhYb7KcVE8awImHT5kQSL7K8yJM5O9CZC9Kf8-NVSjuR9RU&usqp=CAU",
+        
+        componentes: [
+            {
+            papelgUnd :30, 
+            plastgUnd : 103, 
+            vidriogUnd: 60, 
+            metalgUnd: 80, 
+            otrogUnd: 13
+        }
+        ],
+        consumos:[{
+            2018 : 6000,
+            2019 : 13600,
+            2020 : 3400,
+            2021 : 2340,
+            2022 : 7500,
+            2023 : 8700,
+            2024 : 2400
+            
+        }]
+    },
+
+    
+    
+    {
+        codigo :"Emp3",
+        nombre: "Envase 4",
+        descripcion:"Cartón impreso dentro de bolsa termosellada transparente de PP",
+        linkImagen: "https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/sodimacCO/554230/w=1068,h=832,f=webp,fit=contain,q=85",
+        
+        componentes: [
+            {
+            papelgUnd :45, 
+            plastgUnd : 123, 
+            vidriogUnd: 0, 
+            metalgUnd: 0, 
+            otrogUnd: 12
+        }
+        ],
+        consumos:[{
+            2018 : 7000,
+            2019 : 14700,
+            2020 : 3400,
+            2021 : 2340,
+            2022 : 7500,
+            2023 : 8700,
+            2024 : 2400
+            
+        }]
+    },
+
+    {
+        codigo :"Emp5",
+        nombre: "Envase 5",
+        descripcion:"Cartón impreso dentro de bolsa termosellada transparente de PP",
+        linkImagen: "https://exitocol.vtexassets.com/arquivos/ids/5806397/barredor-burlete-flexible-para-puerta-de-garaje-x3mt-max-hogar-negro.jpg?v=637484152907870000",
+        
+        componentes: [
+            {
+            papelgUnd :55, 
+            plastgUnd : 133, 
+            vidriogUnd: 0, 
+            metalgUnd: 0, 
+            otrogUnd: 12
+        }
+        ],
+        consumos:[{
+            2018 : 8000,
+            2019 : 15000,
+            2020 : 3400,
+            2021 : 2340,
+            2022 : 7500,
+            2023 : 8700,
+            2024 : 2400
+            
+        }]
+    },
+
+
+    
+    {codigo:"Emp4567",nombre:"Empaque Barredor Flex",descripcion:"Blister plastico PVC transparente con cartón impreso, sellado con cinta pegante",linkImagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxFJJ2UIrrFwA7SteuAgm9RtHPQVtVtIHcFQ&s",
+            componentes:[{
+            
+            papelgUnd:65,
+            plastgUnd : 140,
+            vidriogUnd: 0,
+            metalgUnd: 0,
+            otrogUnd: 0}],
+
+            consumos:[{
+                2018 : 9000,
+                2019 : 16500,
+                2020 : 3400,
+                2021 : 2340,
+                2022 : 7500,
+                2023 : 8700,
+                2024 : 2400
+                
+            }]
+        
+        },
+
+            {codigo:"Emp7",nombre:"Caja de cartón Pequeña 1",descripcion:"Caja de cartón empaque secundario Se cierra con cinta plástica de 2 Pulgadas de ancho",linkImagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6tCrpivHqLKYMBuxe57DGlhYb7KcVE8awImHT5kQSL7K8yJM5O9CZC9Kf8-NVSjuR9RU&usqp=CAU",componentes:[{papelgUnd:580,
+                plastgUnd : 70,
+                vidriogUnd: 150,
+                metalgUnd: 0,
+                otrogUnd: 0}],
+                consumos:[{
+                    2018 : 10000,
+                    2019 : 17500,
+                    2020 : 3400,
+                    2021 : 2340,
+                    2022 : 7500,
+                    2023 : 8700,
+                    2024 : 2400
+                    
+                }]},
+
+                {codigo:"EmpSepOll1",nombre:"Empaque Separador Ollas",descripcion:"Bolsa plástica termosellada con impresion en Cartón Maule",linkImagen:"https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/sodimacCO/554230/w=1068,h=832,f=webp,fit=contain,q=85",componentes:[{papelgUnd:30,
+                    plastgUnd : 85,
+                    vidriogUnd: 160,
+                    metalgUnd: 0,
+                    otrogUnd: 0}],
+                    consumos:[{
+                        2018 : 11000,
+                        2019 : 18500,
+                        2020 : 3400,
+                        2021 : 2340,
+                        2022 : 7500,
+                        2023 : 8700,
+                        2024 : 2400
+                        
+                    }]}
+
+      
+                
+]
